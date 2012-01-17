4ac05ccf4788b3afd9b25ce95cfa0164821a645e
Austin King <shout@ozten.com>
2012-01-16 16:32:12 -0800

    Putting track info into a 2 column grid layout. Clicking track name loads track html

--- js/bow.js
+++ js/bow.js
@@ -1,5 +1,11 @@
-$('#track-nav li a').click(function (e) {
-  e.preventDefault();
-  var url = $(this).attr('href');
-  $('#track-view').load(url);
+$(document).ready(function () {
+
+  $('#track-nav li a').click(function (e) {
+    e.preventDefault();
+    $(this).parent().parent().find('.active').removeClass('active');
+    var url = $(this).attr('href');
+    $(this).parent().addClass('active'); 
+    $('#track-view').load(url);
+  });
+  $('#track-nav li a:first').click();
 });
